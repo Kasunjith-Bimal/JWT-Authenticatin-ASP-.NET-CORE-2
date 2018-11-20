a7558cc545dad17ba66a1985c5bf9c8b12b79913
Kasunjith-Bimal <kasunysoft@gmail.com>
2018-11-20 22:27:18 +0530

    Jwt token authentication

---
+++ TokenBasedAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TokenBasedAuthentication.Data;
+using TokenBasedAuthentication.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TokenBasedAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+      [HttpPost]
+      [Route("Login")]
+      public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+      {
+            var user = await userManager.FindByNameAsync(loginModel.UserName);
+            if (user != null && await userManager.CheckPasswordAsync(user, loginModel.Password)){
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                };
+                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySuperSecureKey"));
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://oec.com",
+                    audience: "http://oec.com",
+                    expires: DateTime.UtcNow.AddHours(1),
+                    claims: claims,
+                    signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+      } 
+    }
+}
+
---
+++ TokenBasedAuthentication/Data/SeedDataBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokenBasedAuthentication.Data
+{
+    public class SeedDataBase
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            context.Database.EnsureCreated();
+
+            if (!context.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = "kasunysoft@gmail.com",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "Kasunjith"
+                };
+                userManager.CreateAsync(user, "Kasunysoft123@");
+            }
+        }
+    }
+}
